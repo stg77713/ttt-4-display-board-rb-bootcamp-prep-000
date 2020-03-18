d89347688c8a76fb464b0519f4f3722c76a810a6
George Lugo <stg77713@gmail.com>
2020-03-18 18:40:28 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,5 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+  separator = " | "
 end 
